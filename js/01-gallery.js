87ea5f6083d26309bd07fa2d842989d201c68576
AleksandrVasilko <avasilko7@gmail.com>
2021-11-13 15:45:10 +0200

    add preview gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+/*console.log(galleryItems);*/
+
+const galleryConainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardMarkup(galleryItems);
+
+galleryConainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+
+
+console.log(createGalleryCardMarkup(galleryItems));
+
+function createGalleryCardMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+      return `<a class = "gallery__item" href="${original}"><img src = "${preview}" class = "gallery__image"></a>`
+  }).join('');
+}
