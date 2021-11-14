140a8cfd1e46395f1f53672aeaf4e045a7bac3b6
AleksandrVasilko <avasilko7@gmail.com>
2021-11-15 00:16:49 +0200

    1 Task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,48 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 /*console.log(galleryItems);*/
 
-const galleryConainer = document.querySelector('.gallery');
+let openOriginalImage;
+
+const galleryContainer = document.querySelector('.gallery');
 const cardsMarkup = createGalleryCardMarkup(galleryItems);
 
-galleryConainer.insertAdjacentHTML('beforeend', cardsMarkup);
-
-
-
+galleryContainer.addEventListener('keydown', onPressEscToCloseImage);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 console.log(createGalleryCardMarkup(galleryItems));
 
 function createGalleryCardMarkup(galleryItems) {
   return galleryItems.map(({ preview, original, description }) => {
-      return `<a class = "gallery__item" href="${original}"><img src = "${preview}" class = "gallery__image"></a>`
+      return `<div class="gallery__item">
+             <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`
   }).join('');
+}
+
+galleryContainer.addEventListener('click', event => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    openOriginalImage = basicLightbox.create(`
+		<img src="${event.target.dataset.source}" width="1280" height="900">
+	`)
+        openOriginalImage.show()
+});
+
+function onPressEscToCloseImage(event) {
+  if (event.code !== 'Escape') {
+      return;
+    }
+    openOriginalImage.close();
 }
