3722fc7af0ae3daa1ec186bffb1a87e3beadf3d7
AleksandrVasilko <avasilko7@gmail.com>
2021-11-15 21:01:43 +0200

    Final version

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+let openOriginalImage;
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardMarkup(galleryItems);
+
+galleryContainer.addEventListener('keydown', onPressEscToCloseImage);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+console.log(createGalleryCardMarkup(galleryItems));
+
+function createGalleryCardMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href=${original}>
+                <img class="gallery__image" src=${preview} alt=${description} />
+              </a>`
+    }).join('');
+}
+
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+    scrollZoom: false,
+});
+
+
+function onPressEscToCloseImage(event) {
+    if (event.code !== 'Escape') {
+        return;
+    }
+    openOriginalImage.close();
+}
